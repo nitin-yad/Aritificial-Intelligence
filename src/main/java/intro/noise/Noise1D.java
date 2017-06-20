226aaa3740e38a4c0be8358804fa07d866354a03
nitin-yad <nitsu.006@gmail.com>
2017-06-20 09:42:53 +0530

    updating missing codes

--- src/main/java/intro/noise/Noise1D.java
+++ src/main/java/intro/noise/Noise1D.java
@@ -1,7 +1,33 @@
 package intro.noise;
 
+import processing.core.PApplet;
+
 /**
  * Created by nitin.yadav on 19-06-2017.
  */
-public class Noise1D {
+public class Noise1D extends PApplet {
+
+    float inc = 0.01f;
+    float xOff = 0.0f;
+
+    public void settings(){
+        size(640,360);
+    }
+
+    public void setup(){
+        background(0);
+        noStroke();
+    }
+
+    public void draw(){
+        fill(0,10);
+        rect(0,0,width,height);
+//        float n = random(0, width);
+        float n = noise(xOff)*width;
+        xOff += inc;
+        fill(200);
+        ellipse(n, height/2, 16, 16);
+    }
+
+    public static void main(String[] args){PApplet.main("intro.noise.Noise1D");}
 }
--- src/main/java/intro/walktomouse/MouseWalker.java
+++ src/main/java/intro/walktomouse/MouseWalker.java
@@ -1,7 +1,29 @@
 package intro.walktomouse;
 
+import processing.core.PApplet;
+
 /**
  * Created by nitin.yadav on 17-06-2017.
  */
-public class MouseWalker {
+public class MouseWalker extends PApplet{
+    Walker w;
+
+    public void settings(){
+        size(640,360);
+    }
+
+    public void setup(){
+        frameRate(30);
+        w = new Walker(width, height,this);
+        background(255);
+    }
+
+    public void draw(){
+        w.step();
+        w.display();
+    }
+
+    public static void main(String[] args){
+        PApplet.main("intro.walktomouse.MouseWalker");
+    }
 }
